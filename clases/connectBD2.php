291aff142d3eae02b16a6e7aa8d67a1ca66a7659
astur4 <moriyon@gmail.com>
2016-09-19 20:07:59 +0000

    connect bd

--- clases/connectBD2.php
+++ clases/connectBD2.php
@@ -1,8 +1,14 @@
 <?php
 
-	include_once("config2.php");
+	//include_once("config2.php");
 	
-	$link = mysqli_connect($username, $password, $database);
+	//$link = mysqli_connect($username, $password, $database);
+	
+	$link = mysqli_connect("localhost","wsrveimp","caseyuqi");
+	
+	if ($link){
+		mysqli_select_db("wsrveimp_olloniego",$link);
+	}
 	
 	$link->query("SET NAMES 'utf8'");
 	
