16c5ecc5ae3ed7c7673aede21403f40dad5ed64c
astur4 <moriyon@gmail.com>
2016-09-23 10:57:23 +0000

    update 2

---
+++ Config/config2.php
@@ -0,0 +1,13 @@
+<?php
+
+    //Configuración del proyecto (puertos, usuarios, etc...)
+   
+	// Cambiar las variables según el entorno -- Pendiente
+
+    $servername = getenv('IP');
+    $username = getenv('wsrveimp');
+    $password = "caseyuqi";
+    $database = "wsrveimp_olloniego";
+    $dbport = 3306;
+
+?>
